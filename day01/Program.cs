7748e821560542c0cb9943799d5a46a26ac13beb
Lars Nyström <nystrom.lasse@gmail.com>
2019-11-28 00:47:52 +0100

    Refactor part 1

--- day01/Program.cs
+++ day01/Program.cs
@@ -12,14 +12,19 @@ namespace day01
                 .Where(l => !string.IsNullOrWhiteSpace(l))
                 .ToList();
 
+            Part1(lines[0]);
+        }
+
+        static void Part1(string input)
+        {
             int floor = 0;
-            foreach (var c in lines[0])
+            foreach (var c in input)
             {
                 if (c == '(') floor++;
                 if (c == ')') floor--;
             }
 
-            System.Console.WriteLine($"Stopped at floor {floor}");
+            System.Console.WriteLine($"Part 1: Stopped at floor {floor}");
         }
     }
 }
