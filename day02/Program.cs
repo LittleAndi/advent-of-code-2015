88aefd5aba7cec8a15856dd7b6fea42305f026a9
Lars Nyström <nystrom.lasse@gmail.com>
2019-11-28 01:52:05 +0100

    Add RibbonWrap and RibbonBow

--- day02/Program.cs
+++ day02/Program.cs
@@ -11,12 +11,12 @@ namespace day02
         {
             var presents = File.ReadAllLines("input.txt")
                 .Where(l => !string.IsNullOrWhiteSpace(l))
-                .Select(l => new Present 
-                    {
-                        Length = int.Parse(l.Split('x')[0]),
-                        Width = int.Parse(l.Split('x')[1]),
-                        Height = int.Parse(l.Split('x')[2])
-                    })
+                .Select(l => new Present
+                {
+                    Length = int.Parse(l.Split('x')[0]),
+                    Width = int.Parse(l.Split('x')[1]),
+                    Height = int.Parse(l.Split('x')[2])
+                })
                 .ToList();
 
             Part1(presents);
@@ -37,13 +37,31 @@ namespace day02
         public int WrappingPaper => 2 * Length * Width + 2 * Width * Height + 2 * Height * Length;
         public int Slack
         {
-            get {
-                var sides = new HashSet<int>();
+            get
+            {
+                var sides = new List<int>();
                 sides.Add(Length * Width);
                 sides.Add(Width * Height);
                 sides.Add(Height * Length);
                 return sides.OrderBy(s => s).First();
             }
         }
+
+        public int RibbonWrap
+        {
+            get
+            {
+                var sizes = new List<int>();
+                sizes.Add(Length);
+                sizes.Add(Width);
+                sizes.Add(Height);
+
+                var s1 = sizes.OrderBy(s => s).First();
+                var s2 = sizes.OrderBy(s => s).Skip(1).First();
+
+                return s1 + s1 + s2 + s2;
+            }
+        }
+        public int RibbonBow => Length * Width * Height;
     }
 }
