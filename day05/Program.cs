dcf179a29d9f00b118d794b8da26d035aed9c939
Lars Nyström <nystrom.lasse@gmail.com>
2019-11-30 21:02:34 +0100

    Add more tests

--- day05/Program.cs
+++ day05/Program.cs
@@ -17,6 +17,31 @@ namespace day05
     public class NaughtyOrNice
     {
         public string Input { get; set; }
-        public bool IsNice => false;
+        public bool IsNice => ContainsRepeatingLetters && !ContainsInvalidStrings;
+
+        public bool ContainsRepeatingLetters
+        {
+            get
+            {
+                for (int i = 1; i < Input.Length; i++)
+                {
+                    if (Input[i] == Input[i-1]) return true;
+                }
+                return false;
+            }
+        }
+
+        public bool ContainsInvalidStrings
+        {
+            get
+            {
+                bool containsInvalidChars = false;
+                if (Input.Contains("ab")) containsInvalidChars = true;
+                if (Input.Contains("cd")) containsInvalidChars = true;
+                if (Input.Contains("pq")) containsInvalidChars = true;
+                if (Input.Contains("xy")) containsInvalidChars = true;
+                return containsInvalidChars;
+            }
+        }
     }
 }
--- tests/day05tests/day05tests.cs
+++ tests/day05tests/day05tests.cs
@@ -13,10 +13,34 @@ namespace day05tests
         [InlineData("jchzalrnumimnmhp", false)]
         [InlineData("haegwjzuvuyypxyu", false)]
         [InlineData("dvszwmarrgswjxmb", false)]
-        public void Test1(string input, bool expected)
+        public void TestIsNice(string input, bool expected)
         {
             var non = new NaughtyOrNice{ Input = input };
             non.IsNice.ShouldBe(expected);
+        }
+
+        [Theory]
+        [InlineData("ugknbfddgicrmopn", true)]
+        [InlineData("aaa", true)]
+        [InlineData("jchzalrnumimnmhp", false)]
+        [InlineData("haegwjzuvuyypxyu", true)]
+        [InlineData("dvszwmarrgswjxmb", true)]
+        public void TestRepeatingLetters(string input, bool expected)
+        {
+            var non = new NaughtyOrNice{ Input = input };
+            non.ContainsRepeatingLetters.ShouldBe(expected);
+        }
+
+        [Theory]
+        [InlineData("ugknbfddgicrmopn", false)]
+        [InlineData("aaa", false)]
+        [InlineData("jchzalrnumimnmhp", false)]
+        [InlineData("haegwjzuvuyypxyu", true)]
+        [InlineData("dvszwmarrgswjxmb", false)]
+        public void TestInvalidStrings(string input, bool expected)
+        {
+            var non = new NaughtyOrNice{ Input = input };
+            non.ContainsInvalidStrings.ShouldBe(expected);
         }
     }
 }
